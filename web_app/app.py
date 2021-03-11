46312161a0c5ab228b72af3e7b1852de7d2c1600
christian <christian.stefani@studenti.unitn.it>
2021-03-11 15:52:08 +0100

    commit to save, few things new

--- web_app/app.py
+++ web_app/app.py
@@ -19,11 +19,12 @@ LOADED = False
 
 array = np.array([])
 nm_sens = 8
+COLORS = ['#4dc9f6', '#f67019', '#f53794', '#537bc4', '#acc236', '#166a8f',	'#00a950', '#58595b', '#8549ba']
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Select your data")
 
 
 @app.route('/submit', methods = ['POST'])
@@ -63,14 +64,13 @@ def get_datatime(np_array):
         if hour not in hours:
             hours.append(hour)
     number_sensor = int((len(np_array[0])-3)/3)
-    return render_template("graphics.html", np_array=np_array, days=days, hours=hours, number_sensor=number_sensor, title="graphics")
+    return render_template("graphics.html", np_array=np_array, days=days, hours=hours, number_sensor=number_sensor, color=COLORS, title="Create your chart")
 
 
 def get_data(arr, options, sensor):
     date_from = datetime.datetime.strptime(options["data_from"], '%b %d %Y ') # !!IMPORTANT the data has one space at the end of the line
     date_to = datetime.datetime.strptime(options["data_to"], '%b %d %Y ')
     values = []
-    labels = []
     which_data = []
     name_field = []
     for id in range(len(sensor)):
@@ -84,19 +84,20 @@ def get_data(arr, options, sensor):
             if options.get("volt") is not None:
                 which_data.append(id*3+3)
                 name_field.append("Volt_" + str(id + 1))
-
+    first = True
+    labels = []
     for i in which_data:
         tmp_values = []
-        tmp_labels = []
         id = 1
         for data in arr[1:, i]:
             current_date = datetime.datetime.strptime(arr[id][0], " %b %d %Y %H:%M:%S")
             if date_from.date() <= current_date.date() <= date_to.date():
                 tmp_values.append(data)
-                tmp_labels.append(current_date)
+                if first:
+                    labels.append(current_date)
             id = id + 1
         values.append(tmp_values)
-        labels.append(tmp_labels)
+        first = False
 
     return name_field, values, labels
 
@@ -127,9 +128,9 @@ def plotline():
         "media": media
     }
     names, dv, dl = get_data(array, option, sensor)
-    print("label len: ",len(dl))
+    print("label len: ", len(dl))
     print("value len: ", len(dv))
-    return render_template('lineChart.html', title='line chart', options=option, sensors=sensor, labels=dl[0], values=dv[0], names=names[0])
+    return render_template('lineChart.html', title='line chart', labels=dl, values=dv, names=names, color=COLORS, number_chart=len(dv))
 
 
 if __name__ == '__main__':
