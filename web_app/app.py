04edf538c1f51bc6f2f84e79c17c5be52da3d78a
christian <christian.stefani@studenti.unitn.it>
2021-03-18 16:29:40 +0100

    bug fix

--- web_app/app.py
+++ web_app/app.py
@@ -250,4 +250,4 @@ if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     app.debug = True
-    app.run(host='0.0.0.0', port="5000")
+    app.run(host='0.0.0.0')
